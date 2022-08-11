d815722b36adcc85021611a7f72bafe22896c19f
Matt Decker <matt@Matts-MBP.lan>
2022-08-11 15:00:12 -0400

    express working

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+const PORT = process.env.PORT || 9000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/users', (req, res, next) => {
+    res.json({ message: "api working"})
+})
+
+
+server.use('*', (req, res, next) => {
+    res.send(`<h2>Hello</h2>`)
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
+
+console.log(process.env.PORT)
+
